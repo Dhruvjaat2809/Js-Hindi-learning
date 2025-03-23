69879a97464027cdc7c7c5834f5b82473c038fe6
Dhruv Chaudhary <166792999+Dhruvjaat2809@users.noreply.github.com>
2025-03-23 11:12:21 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "Dhruv@gmail.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2// not allowed 
+
+console.log(accountId);
+
+accountEmail = "lcky@gmail.com"
+accountpassword = "2345"
+accountcity = "gzb"
+let accountstate;
+
+/*
+VAR use nhi krna
+/*
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate])

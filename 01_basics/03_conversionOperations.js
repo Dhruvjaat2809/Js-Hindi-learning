3a33213bd62d926b7f9872d453dcb3fa88f032cd
Dhruv Chaudhary <166792999+Dhruvjaat2809@users.noreply.github.com>
2025-03-24 09:48:23 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+//let score = "33"
+
+//console.log(typeof (score))
+
+//let valueInNumber = Number (score)
+//console.log(typeof (valueInNumber))
+//console.log(valueInNumber)
+
+// "33" = 33
+//"33abd" = NaN
+//true => 1; false => 0
+
+
+
+//let isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=> False
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

8cf77c3dcb28115d4ceafc1cb256e97e40276a96
Dhruv Chaudhary <166792999+Dhruvjaat2809@users.noreply.github.com>
2025-04-07 13:53:15 +0000

    stringconversion.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -46,7 +46,21 @@
 //let str3 = str1 + str2
 //console.log(str3);
 
-console.log("1"+2)
-console.log(1+"2")
-console.log(1+"2"+2)
-console.log(1+2+"2")
+console.log("1"+2) //12
+console.log(1+"2") // 12
+console.log(1+"2"+2) // 122
+console.log(1+2+"2") // 32
+
+console.log(true) //true
+
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gamecounter = 100
+gamecounter++; // 101
+++gamecounter; // 101
+console.log(gamecounter);
+
